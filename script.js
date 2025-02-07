92a6297223766c7a5b231b2bfb124b23f5819660
FernandezValentina <vframos.90@gmail.com>
2025-02-06 23:44:13 -0300

    Se agrega función mostrarPronostico

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ function mostrarClimaActual() {
     let ciudad = prompt("Ingrese el nombre de su ciudad");
     let clima = "25°C con cielo despejado";
     historialConsultas.push("Consulta: Clima en:" + ciudad);
-    
     alert(" El clima en " + ciudad + " es de " + clima);
 }
 mostrarClimaActual();
+
+function mostrarPronostico() {
+    let dias = parseInt(prompt("¿Cuántos días de pronóstico desea ver? (Máximo 5 días)"));
+    const pronostico = ["Soleado","Nublado","Lluvia intensa","Tormenta Eléctrica","Soleado"];
+    let resultado = pronostico.slice(0, dias);
+    historialConsultas.push("Consulta: Pronóstico para " + dias + " días ");
+    alert("Pronóstico para los proximos " + dias + " días:\n" + resultado.join("\n"));
+}
+mostrarPronostico();
