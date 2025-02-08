0b9e197e48f039c832acd93b7d2bec10e122ed85
FernandezValentina <vframos.90@gmail.com>
2025-02-08 14:39:41 -0300

    Se agrega función verHistorial

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const historialConsultas = [];
 
-function mostrarClimaActual() {
-    let ciudad = prompt("Ingrese el nombre de su ciudad");
-    let clima = "25°C con cielo despejado";
-    historialConsultas.push("Consulta: Clima en:" + ciudad);
-    alert(" El clima en " + ciudad + " es de " + clima);
-}
-mostrarClimaActual();
+// function mostrarClimaActual() {
+//     let ciudad = prompt("Ingrese el nombre de su ciudad");
+//     let clima = "25°C con cielo despejado";
+//     historialConsultas.push("Consulta: Clima en:" + ciudad);
+//     alert(" El clima en " + ciudad + " es de " + clima);
+// }
+// mostrarClimaActual();
 
 function mostrarPronostico() {
     let dias;
@@ -26,3 +26,12 @@ function mostrarPronostico() {
     alert("Pronóstico para los proximos " + dias + " días:\n" + resultado.join("\n"));
 }
 mostrarPronostico();
+
+function verHistorial() {
+    if (historialConsultas.length === 0) {
+    alert("El historial de consultas está vacío.");
+    } else {
+        alert("Historial de consultas:\n" + historialConsultas.join("\n"));
+    }
+}
+verHistorial();
