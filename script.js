c1f2beb840d19c90f555a38fc45f1f3aa1b9b1d9
FernandezValentina <vframos.90@gmail.com>
2025-02-08 14:13:57 -0300

    Se agrega validación de entrada y while para que continúe preguntando

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ function mostrarClimaActual() {
 mostrarClimaActual();
 
 function mostrarPronostico() {
-    let dias = parseInt(prompt("¿Cuántos días de pronóstico desea ver? (Máximo 5 días)"));
+    let dias;
+
+    while (true) {
+        dias = parseInt(prompt("¿Cuántos días de pronóstico desea ver? (Máximo 5 días)"));
+    
+     if (!isNaN(dias) && dias >= 1 && dias <= 5) {
+         break;
+    }
+    alert("Por favor, ingresa un número válido entre 1 y 5.");
+  }
     const pronostico = ["Soleado","Nublado","Lluvia intensa","Tormenta Eléctrica","Soleado"];
     let resultado = pronostico.slice(0, dias);
     historialConsultas.push("Consulta: Pronóstico para " + dias + " días ");
+
     alert("Pronóstico para los proximos " + dias + " días:\n" + resultado.join("\n"));
 }
 mostrarPronostico();
